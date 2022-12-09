618b5a7d89e83ec8883389acc100749b4a812588
normanleonor <109060058+normanleonor1961@users.noreply.github.com>
2022-12-09 11:13:26 +0800

    add authentication

--- ourmainapp/app/Http/Controllers/UserController.php
+++ ourmainapp/app/Http/Controllers/UserController.php
@@ -8,6 +8,31 @@ use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
+
+    public function showCorrectHomepage(){
+        if(auth()->check()){
+            return view('homepage-feed');
+        }else{
+            return view('homepage');
+        }
+    }
+
+    public function login(Request $request){
+        $incomingFields = $request->validate([
+            'loginusername' => 'required',
+            'loginpassword' => 'required'
+        ]);
+
+        if(auth()->attempt(['username'=>$incomingFields['loginusername'],'password'=> $incomingFields['loginpassword']])){
+                $request->session()->regenerate();
+                return 'Congrats!!!';
+        }else{
+                return 'Sorry!!!';
+        }
+
+
+    }
+
     public function register(Request $request) {
         $incomingFields = $request->validate([
             'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
